f3002d5f229eb159bc0cea0ab4b0885e7f13e911
henriquepf <henriquepetters@gmail.com>
2019-05-16 23:13:36 -0300

    Fixing some stuff

--- PygameProject1.py
+++ PygameProject1.py
@@ -6,7 +6,7 @@ white = (255, 255, 255)
 grey = (99, 110, 114)
 black = (45, 52, 54)
 red = (214, 48, 49)
-verde = (0, 184, 148)
+verde = (34, 112, 147)
 blue = (9, 132, 227)
 
 #To see if the game initialize properly
@@ -25,6 +25,11 @@ tamanho = 10
 relogio = pygame.time.Clock()
 fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake')
+font = pygame.font.SysFont(None, 25)
+
+def texto(msg, cor):
+    texto1 = font.render(msg, True, cor)
+    fundo.blit(texto1, [largura/4, altura/2])
 
 def cobra(CobraXY):
     for XY in CobraXY:
@@ -34,6 +39,8 @@ def apple(posX, posY):
     pygame.draw.rect(fundo, red, [posX, posY, tamanho, tamanho])
 
 def game():
+    exit = True
+    fimDeJogo = False
     posX = randint(0, (largura - tamanho) / 10) * 10
     posY = randint(0, (altura - tamanho) / 10) * 10
     appleX = randint(0, (largura - tamanho) / 10) * 10
@@ -42,27 +49,40 @@ def game():
     velocidadeY = 0
     CobraXY = []
     CobraComp = 1
-
-    jogo = True
-    while jogo:
+    while exit:
+        while fimDeJogo:
+            fundo.fill(verde)
+            texto("Game Over. "
+                  "To continue press C or E to exit.", red)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit = False
+                    fimDeJogo = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_c:
+                        game()
+                    if event.key == pygame.K_e:
+                        exit = False
+                        fimDeJogo = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and velocidadeX != 1.5:
+                if event.key == pygame.K_LEFT and velocidadeX != tamanho:
                     velocidadeY = 0
-                    velocidadeX = -1.5 #tamanho
-                if event.key == pygame.K_RIGHT and velocidadeX != - 1.5:
+                    velocidadeX = -tamanho
+                if event.key == pygame.K_RIGHT and velocidadeX != - tamanho:
                     velocidadeY = 0
-                    velocidadeX = 1.5 #tamanho
+                    velocidadeX = tamanho
 
-                if event.key == pygame.K_UP and velocidadeY != 1.5:
+                if event.key == pygame.K_UP and velocidadeY != tamanho:
                     velocidadeX = 0
-                    velocidadeY = -1.5 #tamanho
-                if event.key == pygame.K_DOWN and velocidadeY != - 1.5:
+                    velocidadeY = -tamanho
+                if event.key == pygame.K_DOWN and velocidadeY != - tamanho:
                     velocidadeX = 0
-                    velocidadeY = 1.5 #tamanho
+                    velocidadeY = tamanho
 
 
         fundo.fill(black)
@@ -76,28 +96,21 @@ def game():
         CobraXY.append(CobraInicio)
         if len(CobraXY) > CobraComp:
             del CobraXY[0]
+        if any (Bloco == CobraInicio for Bloco in CobraXY[:-1]):
+            fimDeJogo = True
+
+
 
         cobra(CobraXY)
         if posX == appleX and posY == appleY:
             appleX = randint(0, (largura - tamanho) / 10) * 10
             appleY = randint(0, (altura - tamanho) / 10) * 10
-            CobraComp += 1
+            CobraComp += 2
 
         apple(appleX, appleY)
         pygame.display.update()
-        relogio.tick(120)
+        relogio.tick(20)
         #No border
-        if posX > largura:
-            posX = 0
-        if posX < 0:
-            posX = largura - tamanho
-
-        if posY > altura:
-            posY = 0
-        if posY < 0:
-            posY = altura - tamanho
-
-        #WITH border
         # if posX > largura:
         #     posX = 0
         # if posX < 0:
@@ -107,6 +120,17 @@ def game():
         #     posY = 0
         # if posY < 0:
         #     posY = altura - tamanho
+
+        #With border
+        if posX > largura:
+            fimDeJogo = True
+        if posX < 0:
+            fimDeJogo = True
+
+        if posY > altura:
+            fimDeJogo = True
+        if posY < 0:
+            fimDeJogo = True
 
 game()
 pygame.quit()
