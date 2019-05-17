f34314b49fc424070f2b82495ef97cac5eb6df11
henriquepf <henriquepetters@gmail.com>
2019-05-17 08:48:56 -0300

    Addition of features

--- PygameProject1.py
+++ PygameProject1.py
@@ -18,6 +18,7 @@ except:
 #Dimensions of the window
 largura = 640
 altura = 480
+placar = 40
 
 #Position and size of the snake
 tamanho = 10
@@ -25,11 +26,11 @@ tamanho = 10
 relogio = pygame.time.Clock()
 fundo = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Snake')
-font = pygame.font.SysFont(None, 25)
 
-def texto(msg, cor):
+def texto(msg, cor, tam, x, y):
+    font = pygame.font.SysFont(None, 25)
     texto1 = font.render(msg, True, cor)
-    fundo.blit(texto1, [largura/4, altura/2])
+    fundo.blit(texto1, [x, y])
 
 def cobra(CobraXY):
     for XY in CobraXY:
@@ -42,19 +43,17 @@ def game():
     exit = True
     fimDeJogo = False
     posX = randrange(0, largura - tamanho, 10)
-    posY = randrange(0, altura - tamanho, 10)
+    posY = randrange(0, altura - tamanho - placar, 10)
     appleX = randrange(0, largura - tamanho, 10)
-    appleY = randrange(0, altura - tamanho, 10)
+    appleY = randrange(0, altura - tamanho - placar, 10)
     velocidadeX = 0
     velocidadeY = 0
     CobraXY = []
     CobraComp = 1
+    pontos = 0
     while exit:
         while fimDeJogo:
-            fundo.fill(verde)
-            texto("Game Over. "
-                  "To continue press C or E to exit.", red)
-            pygame.display.update()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     exit = False
@@ -64,16 +63,46 @@ def game():
                         exit = True
                         fimDeJogo = False
                         posX = randrange(0, largura - tamanho, 10)
-                        posY = randrange(0, altura - tamanho, 10)
+                        posY = randrange(0, altura - tamanho - placar, 10)
                         appleX = randrange(0, largura - tamanho, 10)
-                        appleY = randrange(0, altura - tamanho, 10)
+                        appleY = randrange(0, altura - tamanho - placar, 10)
                         velocidadeX = 0
                         velocidadeY = 0
                         CobraXY = []
                         CobraComp = 1
+                        pontos = 0
                     if event.key == pygame.K_e:
                         exit = False
                         fimDeJogo = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = pygame.mouse.get_pos()[0]
+                    y = pygame.mouse.get_pos()[1]
+                    print(x, y)
+                    if x > 45 and y > 120 and x < 180 and y < 147:
+                        exit = True
+                        fimDeJogo = False
+                        posX = randrange(0, largura - tamanho, 10)
+                        posY = randrange(0, altura - tamanho - placar, 10)
+                        appleX = randrange(0, largura - tamanho, 10)
+                        appleY = randrange(0, altura - tamanho - placar, 10)
+                        velocidadeX = 0
+                        velocidadeY = 0
+                        CobraXY = []
+                        CobraComp = 1
+                        pontos = 0
+                    elif x > 190 and y > 120 and x < 265 and y < 147:
+                        exit = False
+                        fimDeJogo = False
+            fundo.fill(verde)
+            texto("Game Over.", red, 15, 65, 30)
+            texto("Pontuação Final: " + str(pontos), red, 30, 70, 80)
+            pygame.draw.rect(fundo, blue, [45, 120, 135, 27])
+            texto('Continue(C)', white, 30, 50, 125)
+
+            pygame.draw.rect(fundo, blue, [190, 120, 75, 27])
+            texto('Exit(E)', white, 30, 195, 125)
+
+            pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -92,6 +121,8 @@ def game():
                 if event.key == pygame.K_DOWN and velocidadeY != - tamanho:
                     velocidadeX = 0
                     velocidadeY = tamanho
+                if event.key == pygame.K_SPACE:
+                    CobraComp += 2
 
         if exit:
             fundo.fill(black)
@@ -100,30 +131,31 @@ def game():
 
             if posX == appleX and posY == appleY:
                 appleX = randrange(0, largura - tamanho, 10)
-                appleY = randrange(0, altura - tamanho, 10)
+                appleY = randrange(0, altura - tamanho - placar, 10)
                 CobraComp += 2
+                pontos += 1
 
             # No border
-            # if posX > largura:
-            #     posX = 0
-            # if posX < 0:
-            #     posX = largura - tamanho
-            #
-            # if posY > altura:
-            #     posY = 0
-            # if posY < 0:
-            #     posY = altura - tamanho
+            if posX + tamanho > largura:
+                 posX = 0
+            if posX < 0:
+                 posX = largura - tamanho
+
+            if posY + tamanho > altura - placar:
+                 posY = 0
+            if posY < 0:
+                 posY = altura - tamanho - placar
 
             # With border
-            if posX > largura:
-                fimDeJogo = True
-            if posX < 0:
-                fimDeJogo = True
-
-            if posY > altura:
-                fimDeJogo = True
-            if posY < 0:
-                fimDeJogo = True
+            # if posX + tamanho > largura:
+            #     fimDeJogo = True
+            # if posX < 0:
+            #     fimDeJogo = True
+            #
+            # if posY + tamanho > altura:
+            #     fimDeJogo = True
+            # if posY < 0:
+            #     fimDeJogo = True
 
             CobraInicio = []
 
@@ -135,6 +167,8 @@ def game():
             if any (Bloco == CobraInicio for Bloco in CobraXY[:-1]):
                 fimDeJogo = True
 
+            pygame.draw.rect(fundo, blue, [0, altura-placar, largura, 40])
+            texto('Pontuação: ' + str(pontos), white, 20, 10, altura - 30)
 
 
             cobra(CobraXY)
